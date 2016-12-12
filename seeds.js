b11ff8eb30c581d69be20eaa0d866cfeaab7f670
Brendan Pete <brendan.m.pete@gmail.com>
2016-12-12 12:28:45 -0500

    Reseeded with 6 products.

--- seeds.js
+++ seeds.js
@@ -5,25 +5,44 @@ mongoose.connect('localhost:27017/DevShop');
 
 var products = [
     new Product({
-        imagePath: "https://downloads.2kgames.com/civilization/civvi_fb_share.jpg",
+        imagePath: "http://i.imgur.com/20jolrc.jpg",
         title: "Civiliation 6",
         description: "Sid Meier's Civilization VI is a turn-based 4X video game and the sixth main title in the Civilization series. Civilization VI was developed by Firaxis Games, published by 2K Games, and distributed by Take-Two Interactive.",
         price: 60
     }),
     new Product({
-        imagePath: "https://d1r7xvmnymv7kg.cloudfront.net/sites_products/darksouls3/assets/img/DARKSOUL_facebook_mini.jpg",
+        imagePath: "http://i.imgur.com/ZCdOoug.jpg",
         title: "Dark Souls 3",
         description: "Dark Souls III is an action role-playing video game developed by FromSoftware and published by Bandai Namco Entertainment for PlayStation 4, Xbox One, and Microsoft Windows.",
         price: 60
     }),
     new Product({
-        imagePath: "https://overwatch-a.akamaihd.net/img/logos/overwatch-share-3d5a268515283007bdf3452e877adac466d579f4b44abbd05aa0a98aba582eeaebc4541f1154e57ec5a43693345bebda953381a7b75b58adbd29d3f3eb439ad2.jpg",
+        imagePath: "http://i.imgur.com/LoWMBPd.jpg",
         title: "Overwatch",
-        description: "Overwatch is a team-based multiplayer first-person shooter video game developed and published by Blizzard Entertainment. It was released in May 2016 for Microsoft Windows, PlayStation 4, and Xbox One. ",
+        description: "Overwatch is a team-based multiplayer first-person shooter video game developed and published by Blizzard Entertainment. It was released in May 2016 for Microsoft Windows, PlayStation 4, and Xbox One.",
         price: 60
     }),
-
+    new Product({
+        imagePath: "http://i.imgur.com/OvuOduu.jpg",
+        title: "Steep",
+        description: "Steep is an open world extreme sports video game developed by Ubisoft Annecy and published by Ubisoft for Microsoft Windows, PlayStation 4 and Xbox One. ",
+        price: 60
+    }),
+    new Product({
+        imagePath: "http://i.imgur.com/HH6tKc2.jpg",
+        title: "Battlefield 1",
+        description: "Battlefield 1 is a first-person shooter video game developed by EA DICE and published by Electronic Arts.",
+        price: 60
+    }),
+    new Product({
+        imagePath: "http://i.imgur.com/9xKjoKM.jpg",
+        title: "Dead Rising 4",
+        description: "Dead Rising 4 is an open world survival horror beat 'em up video game developed by Capcom Vancouver and published by Microsoft Studios for Microsoft Windows and Xbox One.",
+        price: 60
+    })
 ];
+
+
 
 var done = 0;
 for (var i = 0; i < products.length; i++) {
