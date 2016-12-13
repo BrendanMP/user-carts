6fb650cf0279cffe5cb781ebea6456d560c2cf78
Brendan Pete <brendan.m.pete@gmail.com>
2016-12-12 20:36:29 -0500

    Added user models

--- server.js
+++ server.js
@@ -1,18 +1,23 @@
 var express = require('express');
-var mongoose = require('mongoose');
 var expressHBS = require('express-handlebars');
+var session = require('express-session');
+var mongoose = require('mongoose');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var passport = require('passport');
+var flash = require('connect-flash');
 
 var index = require('./routes/index');
 var admin = require('./routes/admin');
+var about = require('./routes/about');
 
 var app = express();
 
 mongoose.connect('mongodb://localhost:27017/DevShop');
+require('./config/passport');
 
 // view engine setup
 app.engine('.hbs', expressHBS({
@@ -28,6 +33,10 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
+app.use(session({secret: 'secretSession', resave: false, saveUninitialized: false}));
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(require('node-sass-middleware')({
     src: path.join(__dirname, 'public'),
     dest: path.join(__dirname, 'public'),
@@ -38,6 +47,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/admin', admin);
+app.use('/about', about);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
