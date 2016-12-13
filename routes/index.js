df208df5d5158ccf2f84759d056704b7d5657b83
Brendan Pete <brendan.m.pete@gmail.com>
2016-12-13 14:18:31 -0500

    Basic add to cart functionality

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,26 @@
 var express = require('express');
 var router = express.Router();
-
 var Product = require('../models/product');
+var Cart = require('../models/cart');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
     Product.find(function(err, docs) {
         res.render('index', {title: 'Products', products: docs})
+    });
+});
+
+router.get('/add-to-cart/:id', function (req, res, next) {
+    var productId = req.params.id;
+    var cart = new Cart(req.session.cart ? req.session.cart : {});
+    Product.findById(productId, function(err, product) {
+        if (err) {
+            return res.redirect('/');
+        }
+        cart.add(product, product.id);
+        req.session.cart = cart;
+        res.redirect('/');
+        console.log(req.session.cart);
     });
 });
 
--- routes/user.js
+++ routes/user.js
@@ -28,7 +28,7 @@ router.post('/signup', passport.authenticate('local.signup', {
 
 router.get('/signup', function (req, res, next) {
     var messages = req.flash('error');
-    res.render('user/signup',{
+    res.render('user/signup', {
         title: 'Sign Up',
         csrfToken: req.csrfToken(),
         messages: messages,
@@ -45,7 +45,7 @@ router.post('/login', passport.authenticate('local.login', {
 
 router.get('/login', function (req, res, next) {
     var messages = req.flash('error');
-    res.render('user/login',{
+    res.render('user/login', {
         title: 'Login',
         csrfToken: req.csrfToken(),
         messages: messages,
@@ -56,14 +56,14 @@ router.get('/login', function (req, res, next) {
 module.exports = router;
 
 function isLoggedIn(req, res, next) {
-    if(req.isAuthenticated()) {
+    if (req.isAuthenticated()) {
         return next();
     }
     res.redirect('/')
 }
 
 function notLoggedIn(req, res, next) {
-    if(!req.isAuthenticated()) {
+    if (!req.isAuthenticated()) {
         return next();
     }
     res.redirect('/')
