9d84d71198d30773b14c5c1abcbad5153d8d6cc9
Brendan Pete <brendan.m.pete@gmail.com>
2016-12-13 11:05:02 -0500

    Added heroku env

--- server.js
+++ server.js
@@ -18,7 +18,24 @@ var about = require('./routes/about');
 
 var app = express();
 
-mongoose.connect('mongodb://localhost:27017/DevShop');
+// Connect to database
+if (process.env.MONGODB_URI) {
+    mongoose.connect(process.env.MONGODB_URI);
+}
+else {
+    mongoose.connect('mongodb://localhost:27017/DevShop');
+}
+
+mongoose.connection.on('error', function(err) {
+        console.error('MongoDB connection error: ' + err);
+        process.exit(-1);
+    }
+);
+
+mongoose.connection.once('open', function() {
+    console.log("Mongoose has connected to MongoDB!");
+});
+
 require('./config/passport');
 
 // view engine setup
