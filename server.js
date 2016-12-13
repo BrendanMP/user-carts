8b598ad1bd69bed21274f79b8d4df8bbec1a5743
Brendan Pete <brendan.m.pete@gmail.com>
2016-12-13 13:19:43 -0500

    Added user sessions

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ var bodyParser = require('body-parser');
 var passport = require('passport');
 var flash = require('connect-flash');
 var validator = require('express-validator');
+var MongoStore = require('connect-mongo')(session);
 
 var index = require('./routes/index');
 var userRoutes = require('./routes/user');
@@ -24,13 +25,11 @@ if (process.env.MONGODB_URI) {
 } else {
     mongoose.connect('mongodb://localhost:27017/DevShop');
 }
-
 mongoose.connection.on('error', function(err) {
         console.error('MongoDB connection error: ' + err);
         process.exit(-1);
     }
 );
-
 mongoose.connection.once('open', function() {
     console.log("Mongoose has connected to MongoDB!");
 });
@@ -52,7 +51,13 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(validator());
 app.use(cookieParser());
-app.use(session({secret: 'secretSession', resave: false, saveUninitialized: false}));
+app.use(session({
+    secret: 'secretSession',
+    resave: false,
+    saveUninitialized: false,
+    store: new MongoStore({ mongooseConnection: mongoose.connection }),
+    cookie: { maxAge: 180 * 60 * 1000 }
+}));
 app.use(flash());
 app.use(passport.initialize());
 app.use(passport.session());
@@ -66,6 +71,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(function (req, res, next) {
     res.locals.login = req.isAuthenticated();
+    res.locals.session = req.session;
     next();
 });
 
